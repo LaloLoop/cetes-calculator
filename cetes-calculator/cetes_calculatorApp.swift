79f4a494fd4cad9b6b1bb8f2c3e550f7d7904369
Eduardo Gutierrez Silva <eduardo@yalo.com>
2023-01-02 23:08:09 -0500

    Initial Commit

---
+++ cetes-calculator/cetes_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  cetes_calculatorApp.swift
+//  cetes-calculator
+//
+//  Created by Eduardo Gutiérrez on 2023-01-02.
+//
+
+import SwiftUI
+
+@main
+struct cetes_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
